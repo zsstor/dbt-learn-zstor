24aa893b17866ee14c5b288d04503c6e429cc619
Zack Stor <zstor@yearup.org>
2020-05-27 17:39:58 +0000

    add orders body

--- models/orders.sql
+++ models/orders.sql
@@ -1,10 +1,15 @@
 with payments as (
     select * from {{ ref('stg_payments') }}
+),
+orders as (
+    select * from {{ ref('stg_orders') }}
 )
 
 select
-    id,
-    order_id,
-    sum(amount) as amount
-from payments
+    orders.order_id,
+    orders.customer_id,
+    sum(payments.amount) as amount
+from            orders
+left outer join payments
+    on orders.order_id = payments.order_id
 group by 1, 2
--- models/stg_payments.sql
+++ models/stg_payments.sql
@@ -1,5 +1,5 @@
 select
-    id,
+    id as payment_id,
     "orderID" as order_id,
     amount
 from raw.stripe.payment
