e70a766a5dab3d0107dd7573073dcc88182ec881
Zack Stor <zstor@yearup.org>
2020-05-27 17:53:53 +0000

    resolving lifetime payment bug

--- models/customers.sql
+++ models/customers.sql
@@ -29,7 +29,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         lifetime_payments.total as lifetime_value
     from customers
     left join customer_orders on (customers.customer_id = customer_orders.customer_id)
